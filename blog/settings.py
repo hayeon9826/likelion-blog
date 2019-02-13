a3af9a4cd8b2f230a23a194d528a26bceca604e5
Hayeon <hayeon226@likelion.org>
2019-02-14 04:09:10 +0900

    gitignore 추가

--- blog/settings.py
+++ blog/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v(m+i+73+!)vg*xxoi+o-bfzs5jk3i2c84_7!5*4p6hu2wq-nl'
+#SECRET_KEY = 'v(m+i+73+!)vg*xxoi+o-bfzs5jk3i2c84_7!5*4p6hu2wq-nl'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'v(m+i+73+!)vg*xxoi+o-bfzs5jk3i2c84_7!5*4p6hu2wq-nl')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
